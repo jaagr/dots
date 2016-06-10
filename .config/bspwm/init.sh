c67127d3476c5ebd01079018a1fd0ae86453ad7c
Michael Carlberg <c@rlberg.se>
2016-06-10 09:00:17 +0200

    bspwm: init

---
+++ .config/bspwm/init.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# Launches bspwm
+#
+
+# Append timestamp to both logs
+timestamp=$(date +'%Y-%m-%d %H:%M:%S')
+
+echo -e "---- STARTUP $timestamp ----" >> "$XDG_CACHE_HOME/bspwm/stdout"
+echo -e "---- STARTUP $timestamp ----" >> "$XDG_CACHE_HOME/bspwm/stderr"
+
+"$LOCAL_ETC/sxhkd/init.sh" "$LOCAL_ETC/sxhkd/sxhkdrc.bspwm" &
+{ sleep 1; "$LOCAL_ETC/compton/init.sh" & } &
+
+run_count=0
+
+while true; do
+  run_count=$((run_count+1))
+  export BSPWM_COUNTER=$run_count
+  bspwm > "$XDG_CACHE_HOME/bspwm/stdout" 2> "$XDG_CACHE_HOME/bspwm/stderr" || break
+  killall -q bspc
+done
+
+# Kill off any children
+ps x | egrep "(bspwmrc|lemonbar|lemonbuddy)" | awk '!/grep/ {print $1}' | xargs kill
