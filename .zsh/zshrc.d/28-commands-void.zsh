28275ecaca0155db499c36c9f14d4a50b763ff32
Michael Carlberg <c@rlberg.se>
2016-06-21 11:49:39 +0200

    zsh: kernel cleanup command

---
+++ .zsh/zshrc.d/28-commands-void.zsh
@@ -0,0 +1,8 @@
+function void-clean-kernels
+{
+  local -i keep_count=${1:-4}
+  print "Clearing old kernels... ignoring the ${keep_count} latest"
+  for kernel in $(vkpurge list | head -n-${keep_count}); do
+    sudo vkpurge rm "$kernel"
+  done
+}
