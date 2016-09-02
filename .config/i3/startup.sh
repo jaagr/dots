c02932193d9f6edcd983afb92880bfb931403221
Michael Carlberg <c@rlberg.se>
2016-09-02 11:25:39 +0200

    refactor(lemonbuddy): Startup script

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-source bootstrap.sh
-
-include utils/x11.sh
-
-bootstrap::finish
+ln -fs "$(i3 --get-socketpath)" "$XDG_CONFIG_HOME/i3/socket"
 
 killall -q lemonbuddy lemonbar xdrawrect
 
@@ -25,50 +21,9 @@ fi
 [[ "$size_gap" ]] && i3-msg gaps outer all set "$size_gap"
 [[ "$size_border" ]] && i3-msg border pixel "$size_border"
 
-config="${lemonbuddy_config:-${LOCAL_ETC}/lemonbuddy/i3.conf}"
-
-for bar in "${lemonbuddy_bars[@]}"; do
-  echo "Using lemonbuddy config: $config"
-
-  monitor="$(lemonbuddy "$bar" -c "$config" --dump=monitor)"
-
-  if ! x11::monitor_connected "$monitor"; then
-    echo "Skipping lemonbuddy bar '$bar' for disconnected monitor '$monitor'" >&2; continue
-  fi
-
-  bar_wmname=$(lemonbuddy "$bar" -c "$config" --print-wmname)
-
-  if [[ -z "$bar_wmname" ]]; then
-    echo "Could not get Lemonbuddy WM_NAME for bar '$bar', skipping..." >&2; continue
-  fi
-
-  if lemonbuddy -h | grep -q "pipe=FILE"; then
-    lemonbuddy_wrapper "$bar" -c "$config" &
-  else
-    lemonbuddy "$bar" -c "$config" &
-  fi
-
-  if lemonbuddy -h | grep -q "restack"; then
-    continue
-  fi
-
-  retries=0
-
-  while (( retries++ < 10 )); do
-    root_id=$(x11::wmname_wid "[i3 con] output $monitor")
-    bar_id=$(x11::wmname_wid "$bar_wmname")
-
-    if [[ "$root_id" ]] && [[ "$bar_id" ]]; then
-      xdo above -t "$root_id" "$bar_id"
-      break
-    else
-      printf "Failed to restack lemonbuddy window (root=%s bar=%s)... retrying in 1s\n" "$root_id" "$bar_id" >&2
-      sleep 1
-    fi
-  done
-done
-
-ln -fs "$(i3 --get-socketpath)" "$XDG_CONFIG_HOME/i3/socket"
+"${XDG_CONFIG_HOME}"/lemonbuddy/launch.sh --kill-all \
+  "${lemonbuddy_config:-${XDG_CONFIG_HOME}/lemonbuddy/i3.conf}" \
+  "${lemonbuddy_bars[*]}"
 
 if [[ -n "$(type theme::post_startup 2>/dev/null)" ]]; then
   theme::post_startup 2>/dev/null &
