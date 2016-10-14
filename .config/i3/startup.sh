816c6aeb42dc4c0f57c464e2f7413e51e7b413af
Michael Carlberg <c@rlberg.se>
2016-10-14 13:51:52 +0200

    i3: update

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
 
-ln -fs "$(i3 --get-socketpath)" "$XDG_CONFIG_HOME/i3/socket"
+source bootstrap.sh
 
-killall -q lemonbuddy lemonbar xdrawrect
+include utils/x11.sh
 
-[[ -e "$CURRENT_THEME/i3" ]] && {
-  source "$CURRENT_THEME/i3"
+bootstrap::finish
+
+function main
+{
+  killall -q lemonbuddy lemonbar xdrawrect
+  ln -fs "$(i3 --get-socketpath)" "${XDG_CONFIG_HOME}/i3/socket"
+
+  [[ -e ${CURRENT_THEME}/i3 ]] && source "${CURRENT_THEME}/i3"
+
+  declare -f theme::pre_startup >/dev/null && theme::pre_startup
+
+  [[ "$size_gap" ]] && i3-msg gaps outer all set "$size_gap"
+  [[ "$size_border" ]] && i3-msg border pixel "$size_border"
+
+  "${XDG_CONFIG_HOME}/lemonbuddy/launch" --kill-all \
+    "${lemonbuddy_config:-${XDG_CONFIG_HOME}/lemonbuddy/config.i3}" \
+    "${lemonbuddy_bars[*]}"
+
+  declare -f theme::post_startup >/dev/null && theme::post_startup
 }
 
-if [[ -n "$(type theme::pre_startup 2>/dev/null)" ]]; then
-  theme::pre_startup 2>/dev/null & sleep 1
-fi
-
-if [ -s "$HOME/.fehbg" ]; then
-  source "$HOME/.fehbg" 2>/dev/null
-elif [ -e "$HOME/.wallpapers/1.png" ]; then
-  feh --bg-center --image-bg -B black "$HOME/.wallpapers/1.png"
-fi
-
-[[ "$size_gap" ]] && i3-msg gaps outer all set "$size_gap"
-[[ "$size_border" ]] && i3-msg border pixel "$size_border"
-
-"${XDG_CONFIG_HOME}"/lemonbuddy/launch.sh --kill-all \
-  "${lemonbuddy_config:-${XDG_CONFIG_HOME}/lemonbuddy/i3.conf}" \
-  "${lemonbuddy_bars[*]}"
-
-if [[ -n "$(type theme::post_startup 2>/dev/null)" ]]; then
-  theme::post_startup 2>/dev/null &
-fi
+main "$@"
